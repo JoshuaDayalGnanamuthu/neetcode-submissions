bbe6d1ed97257a9f40d8bf635246c18aa41f7156
Joshua Dayal <joshuadayal72@gmail.com>
2026-09-14 14:00:42 -0400

    Add: anagram-groups - submission-20

---
+++ Data Structures & Algorithms/anagram-groups/submission-20.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        def encoding(string :str) -> str:
+            chars = [0 for i in range(26)]
+            for i in range(len(string)):
+                chars[ord(string[i]) - 97]+=1
+            
+            chars = [str(x) for x in chars]
+            return ",".join(chars)
+
+        map = defaultdict(list)
+
+        for string in strs:
+            map[encoding(string)].append(string)
+        
+        results = []
+
+        for key, values in map.items():
+            results.append(values)
+        
+        return results
+
+        
