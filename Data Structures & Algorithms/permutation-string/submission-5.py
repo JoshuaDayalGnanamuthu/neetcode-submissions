a771cf55786a563e135876de9f31e2a845d260d3
Joshua Dayal <joshuadayal72@gmail.com>
2026-09-24 00:15:31 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if (len(s1) > len(s2)):
+            return False
+
+        from collections import Counter
+
+        frequencies = Counter(s1)
+        counter = Counter()
+
+        left = 0
+
+        counter[s2[left]] += 1
+
+        for right in range(1, len(s2)):
+            if (counter == frequencies):
+                return True
+            
+            counter[s2[right]] += 1
+
+            if (right - left + 1 != len(s1)):
+                continue
+            
+            else:
+                if (counter == frequencies):
+                    return True
+                else:
+                    counter[s2[left]] -= 1
+                    left += 1
+        
+        return (counter == frequencies)
+
