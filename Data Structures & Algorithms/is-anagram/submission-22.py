3368be6eb3b36524453e1096e9106a4e610d627d
Joshua Dayal <joshuadayal72@gmail.com>
2026-09-25 09:17:35 -0400

    Add: is-anagram - submission-22

---
+++ Data Structures & Algorithms/is-anagram/submission-22.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        chars = [0] * 26
+
+        for i in range(len(s)):
+            chars[ord(s[i]) - 97] += 1
+            chars[ord(t[i]) - 97] -= 1
+
+        for i in chars:
+            if i:
+                return False
+        
+        return True
+        
